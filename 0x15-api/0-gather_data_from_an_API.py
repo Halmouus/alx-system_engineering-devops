6580635e22efd1f229e9e3163b184683eeb1d284
Halmouus <javierminimaz@gmail.com>
2023-10-24 02:42:07 +0100

    adjustments

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,34 +2,35 @@
 """ script that, using this REST API,
 for a given employee ID, returns information
 about his/her todo list progress"""
-import requests
 import json
+import requests
 import sys
 
-idVar = sys.argv[1]
-user_data = requests.get(f'https://jsonplaceholder.typicode.com/users/{idVar}')
+if __name__ == "__main__":
+    idVar = sys.argv[1]
+    resp = requests.get(f'https://jsonplaceholder.typicode.com/users/{idVar}')
 
-if user_data.status_code == 200:
-    user_info = user_data.json()
-    name = user_info['name']
+    if resp.status_code == 200:
+        user_info = resp.json()
+        name = user_info['name']
 
-todos_data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos_data = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-if todos_data.status_code == 200:
-    title_list = []
-    completed = 0
-    non_completed = 0
+    if todos_data.status_code == 200:
+        title_list = []
+        completed = 0
+        non_completed = 0
 
-    todos = todos_data.json()
-    for todo in todos:
-        if todo["userId"] == int(idVar):
-            title_list.append(todo["title"])
-            if todo["completed"]:
-                completed += 1
-            else:
-                non_completed += 1
+        todos = todos_data.json()
+        for todo in todos:
+            if todo["userId"] == int(idVar):
+                title_list.append(todo["title"])
+                if todo["completed"]:
+                    completed += 1
+                else:
+                    non_completed += 1
 
-    print(f"Employee {name} is done with tasks\
-          ({completed}/{non_completed + completed}):")
-    for i in range(completed):
-        print(f"\t {title_list[i]}")
+        print(f"Employee {name} is done with tasks"
+              f"({completed}/{non_completed + completed}):")
+        for i in range(completed):
+            print(f"\t {title_list[i]}")
