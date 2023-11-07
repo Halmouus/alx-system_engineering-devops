7eb55db8229f804c71f5c8781b278bc3eef9616c
Halmouus <javierminimaz@gmail.com>
2023-11-07 21:36:52 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+fetching the first 10 hot posts listed of a given subreddit
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the first 10
+    hot posts listed for a given subreddit"""
+    headers = {'User-Agent': 'ApiTestALX'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+
+    if resp.status_code == 404:
+        print("None")
+    if resp.status_code == 200:
+        hot = resp.json()
+        titles = []
+        for post in hot['data']['children'][:10]:
+            titles.append(post['data']['title'])
+        for title in titles:
+            print(title)
