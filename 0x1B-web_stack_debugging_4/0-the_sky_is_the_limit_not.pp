d9a587a224f1fd41f2637863d499f8f8d8bff12f
Halmouus <javierminimaz@gmail.com>
2023-11-16 20:01:00 +0100

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,10 +4,10 @@
 exec { 'fix_stack_limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 # restart Nginx
-exec { 'nginx-restart':
+exec { 'nginx_restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
